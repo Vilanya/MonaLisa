161c13652e27f37287affd42606332b64f3b2e81
Vilanya <52811659+Vilanya@users.noreply.github.com>
2019-09-05 01:57:45 +1000

    Add files via upload

--- MonaLisa.ino
+++ MonaLisa.ino
@@ -3,8 +3,6 @@ void setup() {
   pinMode(4, INPUT);
   pinMode(5, INPUT);
   pinMode(6, INPUT);
-
-  
 }
 
 void loop() {
@@ -13,32 +11,26 @@ void loop() {
   bool sensor3 = digitalRead(6);
   bool alarm_on = false;
   
-  if (sensor1 == HIGH)
-  {
+  if (sensor1 == HIGH){
     Serial.println("Active");
     alarm_on = true;
     //Turn alarm on 
   }
 
-  while (alarm_on == false)
-  {
-    if (sensor2 == HIGH)
-    {
+  if (alarm_on == false){
+    
+    if (sensor2 == HIGH){
       Serial.println("Active");
       alarm_on = true;
       //Turn alarm on 
     }
 
-    while (alarm_on == false)
-    {
-      if (sensor3 == HIGH)
-      {
-        Serial.println("Active");
-        alarm_on = true;
-        //Turn alarm on
-      }
+    else if (sensor3 == HIGH){
+      Serial.println("Active");
+      alarm_on = true;
+      //Turn alarm on
     }
   }
-
+  
   delay (1000);
 }
